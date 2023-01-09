c5d71b5ef80d6a180cf899b9a21b88a9f551a95c
phanison898 <phanison898@gmail.com>
2023-01-09 21:05:34 +0530

    created login page

---
+++ components/Login.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import Whatsapp from "../assets/images/whatsapp.png";
+
+const Login = () => {
+  return (
+    <Container>
+      <Image />
+      <LoginButton />
+    </Container>
+  );
+};
+
+export default Login;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.img.attrs((props) => ({
+  src: "https://cdn-icons-png.flaticon.com/512/733/733585.png?w=740&t=st=1673274995~exp=1673275595~hmac=ea5b3e9acfa1370108eeac250e7335a4e6752da402e37c4ae6f14d2a28278ed0",
+}))`
+  width: 160px;
+  height: 160px;
+`;
+
+const LoginButton = styled.button.attrs((props) => ({
+  onclick: props?.onclick,
+}))`
+  width: 250px;
+  height: 50px;
+  background-color: #00d95e;
+  border: none;
+  margin-top: 40px;
+  cursor: pointer;
+  transition: all 0.25s;
+  border-radius: 10px;
+
+  :before {
+    content: "Login with Google";
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  :hover {
+    background-color: #05f76e;
+  }
+`;
